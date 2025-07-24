06a01a5bfdaf7f97275afcd42855c9a0f2502e08
nachochiappero <ignaciogabrielchiappero@gmail.com>
2025-07-23 21:07:32 -0300

    cuarto punto de guardado - listo para ir al origien

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@ export default function Home() {
     <div className="flex justify-center items-center h-screen">
       <button className="p-2 bg-rose-600 rounded-4xl">HOLA COMO ESTAN</button>
       <div className="p-2 bg-rose-600 rounded-4xl"> NUEVO MENSAJITO</div>
+      <div className="p-2 bg-rose-600 rounded-4xl">
+        {" "}
+        NUEVO MENSAJITO PARA EL CUARTO COMMIT
+      </div>
     </div>
   );
 }
